1fdc963a4a14e9e5ab206e87d882f1651e851dfa
rin2yh <hyuki749@gmail.com>
2026-01-21 22:27:56 +0900

    refactor: nvim mini

---
+++ nvim/lua/my/mini.lua
@@ -0,0 +1,255 @@
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.uv.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later -- nowを入れて関数内でエラー発生時にファイル全体の実行を停止しないようにする
+
+now(require('mini.icons').setup)
+now(require('mini.basics').setup)
+
+now(function()
+  require('mini.statusline').setup()
+  vim.opt.laststatus = 3
+  vim.opt.cmdheight = 0
+end)
+
+now(function()
+  require('mini.misc').setup()
+
+  MiniMisc.setup_restore_cursor()
+  vim.api.nvim_create_user_command('Zoom', function()
+    MiniMisc.zoom(0, {})
+  end, { desc = 'Zoom current buffer' })
+  vim.keymap.set('n', 'mz', '<cmd>Zoom<cr>', { desc = 'Zoom current buffer' })
+end)
+
+now(function()
+  require('mini.notify').setup()
+
+  vim.notify = require('mini.notify').make_notify({})
+end)
+
+-- specific highlight pattern
+later(function()
+  local hipatterns = require('mini.hipatterns')
+  local hi_words = require('mini.extra').gen_highlighter.words
+  hipatterns.setup({
+    highlighters = {
+      -- Highlight standalone 'TODO'
+      todo = hi_words({ 'TODO' }, 'MiniHipatternsTodo'),
+      -- Highlight hex color strings (`#rrggbb`) using that color
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
+  })
+end)
+
+later(require('mini.indentscope').setup)
+later(function()
+  require('mini.trailspace').setup()
+  -- require('mini.trailspace').setup()と同じlaterに追加
+  vim.api.nvim_create_user_command(
+    'Trim',
+    function()
+      MiniTrailspace.trim()
+      MiniTrailspace.trim_last_lines()
+    end,
+    { desc = 'Trim trailing space and last blank lines' }
+  )
+end)
+
+-- suggest last opend file
+now(require('mini.starter').setup)
+
+-- vimjp doc
+later(function()
+  add('https://github.com/vim-jp/vimdoc-ja')
+  -- Prefer Japanese as the help language
+  vim.opt.helplang:prepend('ja')
+end)
+
+-- completion
+later(function()
+  require('mini.fuzzy').setup()
+  require('mini.completion').setup({
+    lsp_completion = {
+      process_items = MiniFuzzy.process_lsp_items,
+    },
+  })
+
+  -- improve fallback completion
+  vim.opt.complete = { '.', 'w', 'k', 'b', 'u' }
+  vim.opt.completeopt:append('fuzzy')
+  vim.opt.dictionary:append('/usr/share/dict/words') -- 注意1
+
+  -- define keycodes
+  local keys = {
+    cn = vim.keycode('<c-n>'),
+    cp = vim.keycode('<c-p>'),
+    ct = vim.keycode('<c-t>'),
+    cd = vim.keycode('<c-d>'),
+    cr = vim.keycode('<cr>'),
+    cy = vim.keycode('<c-y>'),
+  }
+
+  -- select by <tab>/<s-tab>
+  vim.keymap.set('i', '<tab>', function()
+    -- popup is visible -> next item
+    -- popup is NOT visible -> add indent
+    return vim.fn.pumvisible() == 1 and keys.cn or keys.ct
+  end, { expr = true, desc = 'Select next item if popup is visible' })
+  vim.keymap.set('i', '<s-tab>', function()
+    -- popup is visible -> previous item
+    -- popup is NOT visible -> remove indent
+    return vim.fn.pumvisible() == 1 and keys.cp or keys.cd
+  end, { expr = true, desc = 'Select previous item if popup is visible' })
+
+  -- complete by <cr>
+  vim.keymap.set('i', '<cr>', function()
+    if vim.fn.pumvisible() == 0 then
+      -- popup is NOT visible -> insert newline
+      return require('mini.pairs').cr() -- 注意2
+    end
+    local item_selected = vim.fn.complete_info()['selected'] ~= -1
+    if item_selected then
+      -- popup is visible and item is selected -> complete item
+      return keys.cy
+    end
+    -- popup is visible but item is NOT selected -> hide popup and insert newline
+    return keys.cy .. keys.cr
+  end, { expr = true, desc = 'Complete current item if item is selected' })
+end)
+
+require('mini.snippets').setup({
+  mappings = {
+    jump_prev = '<c-k>',
+  },
+})
+
+later(function()
+  require('mini.diff').setup()
+end)
+
+later(function()
+  require('mini.git').setup()
+
+  vim.keymap.set({ 'n', 'x' }, '<space>gs', MiniGit.show_at_cursor, { desc = 'Show at cursor' })
+end)
+
+now(function()
+  require('mini.files').setup()
+
+  vim.api.nvim_create_user_command(
+    'Files',
+    function()
+      MiniFiles.open()
+    end,
+    { desc = 'Open file exproler' }
+  )
+end)
+
+later(function()
+  require('mini.pick').setup()
+
+  vim.ui.select = MiniPick.ui_select
+
+  vim.keymap.set('n', '<space>f', function()
+    MiniPick.builtin.files({ tool = 'git' })
+  end, { desc = 'mini.pick.files' })
+end)
+
+later(function()
+  require('mini.jump2d').setup()
+end)
+
+later(function()
+  require('mini.bracketed').setup()
+end)
+
+later(function()
+  require('mini.pairs').setup()
+end)
+
+later(function()
+  local function mode_nx(keys)
+    return { mode = 'n', keys = keys }, { mode = 'x', keys = keys }
+  end
+  local clue = require('mini.clue')
+  clue.setup({
+    triggers = {
+      -- Leader triggers
+      mode_nx('<leader>'),
+
+      -- Built-in completion
+      { mode = 'i', keys = '<c-x>' },
+
+      -- `g` key
+      mode_nx('g'),
+
+      -- Marks
+      mode_nx("'"),
+      mode_nx('`'),
+
+      -- Registers
+      mode_nx('"'),
+      { mode = 'i', keys = '<c-r>' },
+      { mode = 'c', keys = '<c-r>' },
+
+      -- Window commands
+      { mode = 'n', keys = '<c-w>' },
+
+      -- bracketed commands
+      { mode = 'n', keys = '[' },
+      { mode = 'n', keys = ']' },
+
+      -- `z` key
+      mode_nx('z'),
+
+      -- surround
+      mode_nx('s'),
+
+      -- text object
+      { mode = 'x', keys = 'i' },
+      { mode = 'x', keys = 'a' },
+      { mode = 'o', keys = 'i' },
+      { mode = 'o', keys = 'a' },
+
+      -- option toggle (mini.basics)
+      { mode = 'n', keys = 'm' },
+    },
+
+    clues = {
+      -- Enhance this by adding descriptions for <Leader> mapping groups
+      clue.gen_clues.builtin_completion(),
+      clue.gen_clues.g(),
+      clue.gen_clues.marks(),
+      clue.gen_clues.registers({ show_contents = true }),
+      clue.gen_clues.windows({ submode_resize = true, submode_move = true }),
+      clue.gen_clues.z(),
+    },
+  })
+end)
+
+-- disable unused plugin
+now(function()
+  local default_rtp = vim.opt.runtimepath:get()
+  vim.opt.runtimepath:remove(vim.env.VIMRUNTIME)
+  vim.api.nvim_create_autocmd("SourcePre", {
+    pattern = "*/plugin/*",
+    once = true,
+    callback = function()
+      vim.opt.runtimepath = default_rtp
+    end
+  })
+end)
