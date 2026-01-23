28f21545a51b004c1dbe665feacf5edb6bf65be0
rin2yh <hyuki749@gmail.com>
2026-01-23 14:31:11 +0900

    nvim grep

--- config/nvim/lua/my/vim.lua
+++ config/nvim/lua/my/vim.lua
@@ -20,4 +20,32 @@ vim.opt.scrolloff = 3
 -- move the cursor to the previous/next line across the first/last character
 vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
 
+-- use rg for external-grep
+vim.opt.grepprg = table.concat({
+  'rg',
+  '--vimgrep',
+  '--trim',
+  '--hidden',
+  [[--glob='!.git']],
+  [[--glob='!*.lock']],
+  [[--glob='!*-lock.json']],
+  [[--glob='!*generated*']],
+}, ' ')
+vim.opt.grepformat = '%f:%l:%c:%m'
 
+-- ref: `:NewGrep` in `:help grep`
+vim.api.nvim_create_user_command('Grep', function(arg)
+  local grep_cmd = 'silent grep! '
+    .. (arg.bang and '--fixed-strings -- ' or '')
+    .. vim.fn.shellescape(arg.args, true)
+  vim.cmd(grep_cmd)
+  if vim.fn.getqflist({ size = true }).size > 0 then
+    vim.cmd.copen()
+  else
+    vim.notify('no matches found', vim.log.levels.WARN)
+    vim.cmd.cclose()
+  end
+end, { nargs = '+', bang = true, desc = 'Enhounced grep' })
+
+vim.keymap.set('n', '<space>/', ':Grep ', { desc = 'Grep' })
+vim.keymap.set('n', '<space>?', ':Grep <c-r><c-w>', { desc = 'Grep current word' })
