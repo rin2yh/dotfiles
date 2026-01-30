f58f6a138a75bbed93c6fc652f6729b886460e37
rin2yh <hyuki749@gmail.com>
2026-01-30 19:59:46 +0900

    feat(homebrew): add .Brewfile

---
+++ home/.Brewfile
@@ -0,0 +1,12 @@
+brew "mise"
+brew "tree"
+brew "zsh-autosuggestions"
+
+tap "tinygo-org/tools"
+brew "tinygo-org/tools/tinygo"
+
+cask "fuwari"
+cask "ghostty"
+cask "orbstack"
+cask "raycast"
+
--- setup.sh
+++ setup.sh
@@ -9,29 +9,4 @@ else
     echo "Homebrew is already installed."
 fi
 
-# Homebrew経由で他ツールのインストール
-echo "Installing packages via Homebrew..."
-TOOLS=("mise" "ghq" "peco" "zsh-autosuggestions" "tree")
-for tool in "${TOOLS[@]}"; do
-    if ! brew list "$tool" &> /dev/null; then
-        echo "Installing $tool..."
-        brew install "$tool"
-    else
-        echo "$tool is already installed."
-    fi
-done
-
-
-# Homebrew Cask経由でアプリケーションのインストール
-echo "Installing applications via Homebrew Cask..."
-APPS=("ghostty" "raycast" "orbstack" "fuwari" "slack" "discord" "notion" "claude")
-for app in "${APPS[@]}"; do
-    if ! brew list --cask "$app" &> /dev/null; then
-        echo "Installing $app..."
-        brew install --cask "$app"
-    else
-        echo "$app is already installed."
-    fi
-done
-
 echo "Setup completed."
