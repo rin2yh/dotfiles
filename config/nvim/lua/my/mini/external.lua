cb849b8e4b2babdfa2b1fb857ba43310992f0071
rin2yh <hyuki749@gmail.com>
2026-02-04 18:05:52 +0900

    feat(nvim): dockerfile highlight

--- config/nvim/lua/my/mini/external.lua
+++ config/nvim/lua/my/mini/external.lua
@@ -18,7 +18,7 @@ add({
 
 later(function()
   require('nvim-treesitter').install({ 'lua', 'vim', 'tsx', 'go', 'typescript', 'html', 'markdown', 'markdown_inline',
-    'bash', 'terraform', 'hcl' })
+    'bash', 'terraform', 'hcl', 'dockerfile' })
 
   -- 全てのファイルタイプでハイライト有効化
   vim.api.nvim_create_autocmd('FileType', {
