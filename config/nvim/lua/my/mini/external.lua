f1a28e587d8cc20525543f64cd61a27fb8022e18
rin2yh <hyuki749@gmail.com>
2026-02-08 18:15:57 +0900

    feat(nvim): terraform treesitter

--- config/nvim/lua/my/mini/external.lua
+++ config/nvim/lua/my/mini/external.lua
@@ -18,7 +18,7 @@ add({
 
 later(function()
   require('nvim-treesitter').install({ 'lua', 'vim', 'tsx', 'go', 'typescript', 'html', 'markdown', 'markdown_inline',
-    'bash' })
+    'bash', 'terraform', 'hcl' })
 
   -- 全てのファイルタイプでハイライト有効化
   vim.api.nvim_create_autocmd('FileType', {
