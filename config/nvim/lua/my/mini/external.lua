34a310f8a83a8b3d01b3551663bfc08071a4981a
rin2yh <hyuki749@gmail.com>
2026-02-09 16:19:16 +0900

    feat(nvim): diffview

--- config/nvim/lua/my/mini/external.lua
+++ config/nvim/lua/my/mini/external.lua
@@ -30,3 +30,23 @@ later(function()
 end)
 
 add("pcolladosoto/tinygo.nvim")
+
+-- Git Conflict Resolution (VSCode-like 3-way merge)
+later(function()
+  add('sindrets/diffview.nvim') 
+  require('diffview').setup({
+    enhanced_diff_hl = true, -- 差分を詳細にハイライト
+    view = {
+      -- マージ時は自動的に3画面（自分/結果/相手）にする
+      merge_tool = {
+        layout = "diff3_mixed",
+      },
+    },
+  })
+
+  -- 使いやすいようにキーマップを設定
+  -- <leader>gm (Git Merge) でコンフリクト解消画面を開く
+  vim.keymap.set('n', '<leader>gm', '<cmd>DiffviewOpen<cr>', { desc = 'Open Diffview (Merge)' })
+  -- <leader>gx で解消画面を閉じる
+  vim.keymap.set('n', '<leader>gx', '<cmd>DiffviewClose<cr>', { desc = 'Close Diffview' })
+end)
