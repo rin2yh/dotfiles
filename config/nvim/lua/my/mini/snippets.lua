44e95cc907aa623bf3d9f4731f1685d8a320b190
rin2yh <hyuki749@gmail.com>
2026-02-17 17:03:05 +0900

    feat(nvim): Go用スニペットとmini.snippets補完連携を追加
    iferr, forr, testスニペットを定義し、mini.completionの補完ポップアップから展開できるようにした

--- config/nvim/lua/my/mini/snippets.lua
+++ config/nvim/lua/my/mini/snippets.lua
@@ -1,5 +1,11 @@
+local gen_loader = require('mini.snippets').gen_loader
 require('mini.snippets').setup({
+  snippets = {
+    gen_loader.from_lang(),
+  },
   mappings = {
     jump_prev = '<c-k>',
   },
 })
+
+MiniSnippets.start_lsp_server({ match = false })
