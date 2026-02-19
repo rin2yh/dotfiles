fc19bebd313ce02ed1bac4028a1c74df3ffcb832
rin2yh <hyuki749@gmail.com>
2026-02-19 12:27:47 +0900

    fix(nvim): miniを全体で使うので、init.luaに移動し、順番の依存をなくす

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -71,6 +71,23 @@ end, { expr = true })
 -- leader + Code Action で呼び出し
 vim.keymap.set('n', '<space>w', vim.lsp.buf.code_action, { desc = "LSP code action" })
 
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.uv.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
+
 require('my/mini')
 
 -- miniを内部で使用するため、mini以降に配置
