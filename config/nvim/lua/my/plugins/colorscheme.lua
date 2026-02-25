bf4bb437151471a5aee907fb7869e8d9505d1774
rin2yh <hyuki749@gmail.com>
2026-02-25 11:10:19 +0900

    feat(nvim): claerify window separator in tokyonight theme

--- config/nvim/lua/my/plugins/colorscheme.lua
+++ config/nvim/lua/my/plugins/colorscheme.lua
@@ -2,5 +2,14 @@ local now, add = MiniDeps.now, MiniDeps.add
 
 now(function()
   add({ source = 'https://github.com/folke/tokyonight.nvim' })
+  require("tokyonight").setup({
+    style = "storm",
+    on_highlights = function(hl, c)
+      hl.WinSeparator = {
+        fg = c.blue,
+        bold = true,
+      }
+    end,
+  })
   vim.cmd.colorscheme('tokyonight')
 end)
