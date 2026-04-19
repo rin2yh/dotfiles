4aa8229bd8c4aa5a44069d5a864f9e5ca00ce1a6
rin2yh <hyuki749@gmail.com>
2026-04-19 23:51:43 +0900

    refactor(nvim): move lua_ls settings to .luarc.json

--- config/nvim/lua/lsp/lua_ls.lua
+++ config/nvim/lua/lsp/lua_ls.lua
@@ -2,22 +2,6 @@ local M = {}
 
 M.cmd = { 'lua-language-server' }
 M.filetypes = { 'lua' }
-
-M.settings = {
-  Lua = {
-    runtime = {
-      version = 'LuaJIT'
-    },
-    diagnostics = {
-      globals = { 'vim' },
-      -- 未使用変数は冒頭に`_`をつけていれば警告なし
-      unusedLocalExclude = { '_*' }
-    },
-    workspace = {
-        checkThirdParty = false,
-        library = vim.api.nvim_get_runtime_file('lua', true)
-      }
-  }
-}
+M.root_markers = { '.luarc.json', '.luarc.jsonc', '.git' }
 
 return M
