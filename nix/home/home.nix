32255fe4c7d7c74b0893a7cbb73444491a7d9421
林侑生 <yuuki@hayashiyuuseis-MacBook-Air.local>
2026-04-29 00:46:50 +0900

    refactor(nix): home.nix to home/

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -17,12 +17,14 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
+    git
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".gitconfig".source = ./git/.gitconfig;
   };
 
   # Home Manager can also manage your environment variables through
