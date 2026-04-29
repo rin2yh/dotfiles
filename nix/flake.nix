204ed768d6b80ead285ac621872aac5a2b9451ea
rin2yh <hyuki749@gmail.com>
2026-04-29 14:44:14 +0900

    refactor(nix): parameterize username in home configuration

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of yuuki";
+  description = "Home Manager configuration";
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
@@ -15,17 +15,15 @@
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
+      mkHome = username: home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home/home.nix ];
+        extraSpecialArgs = { inherit username; };
+      };
     in
     {
-      homeConfigurations."yuuki" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home/home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      homeConfigurations = {
+        "yuuki" = mkHome "yuuki";
       };
     };
 }
--- nix/home/home.nix
+++ nix/home/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, username, ... }:
 
 let
   dotfiles = "${config.home.homeDirectory}/workspace/dotfiles/nix/home";
@@ -6,8 +6,8 @@ in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "yuuki";
-  home.homeDirectory = "/Users/yuuki";
+  home.username = username;
+  home.homeDirectory = "/Users/${username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
