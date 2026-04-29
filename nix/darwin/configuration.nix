66721e29075319ee7311da0bf7acb72cfa4dc9ad
rin2yh <hyuki749@gmail.com>
2026-04-29 15:49:58 +0900

    feat(nix): add touch_id sudo

--- nix/darwin/configuration.nix
+++ nix/darwin/configuration.nix
@@ -6,4 +6,5 @@
   system.stateVersion = 6;
   # Nix is managed by the Determinate installer; let it own /etc/nix/nix.conf.
   nix.enable = false;
+  security.pam.services.sudo_local.touchIdAuth = true;
 }
