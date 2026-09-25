8063ff4d1e35f44a8cadec52f42910f468bea763
Rinrin <88963450+rin2yh@users.noreply.github.com>
2026-09-25 23:39:32 +0900

    fix: use Command Line Tools clang instead of installing it explicitly

--- home/home.nix
+++ home/home.nix
@@ -25,7 +25,6 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    clang
     dune_3
     emmet-language-server
     fastfetch
